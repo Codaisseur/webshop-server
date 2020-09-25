f2b1d2c6c1fde4048f96e88f37d6e6bd09084c34
svenve-git <svenve@protonmail.com>
2020-09-25 14:14:44 +0200

    added cors middleware

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const app = express();
-const PORT = 4000;
-const orderRouter = require("./routers/order");
-const productRouter = require("./routers/products");
-const categoryRouter = require("./routers/category");
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const PORT = 4000
+const orderRouter = require("./routers/order")
+const productRouter = require("./routers/products")
+const categoryRouter = require("./routers/category")
 
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/orders", orderRouter);
-app.use("/products", productRouter);
-app.use("/categories", categoryRouter);
+app.use("/orders", orderRouter)
+app.use("/products", productRouter)
+app.use("/categories", categoryRouter)
 
-app.listen(PORT, () => console.log("server up and listening"));
+app.listen(PORT, () => console.log("server up and listening"))
