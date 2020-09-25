6ab42874bf04e03d943cf3dc6ff3f4fca1dd3ffb
Matias Garcia <matiasigarcia91@gmail.com>
2020-09-25 11:23:32 +0200

    add product router

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const orderRouter = require("./routers/order");
+const productRouter = require("./routers/product");
 
 app.use(express.json());
 
 app.use("/orders", orderRouter);
+app.use("/products", productRouter);
 
 app.listen(PORT, () => console.log("server up and listening"));
